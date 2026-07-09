014d1c0ac0e3701b109d51d19d05dc90aa64924f
mohammedZ007 <mohdzakkis@gmail.com>
2026-07-09 22:44:40 +0530

    Added a resource block to start/stop ec2 instance from terraform

--- main.tf
+++ main.tf
@@ -14,8 +14,13 @@ resource "aws_instance" "web" {
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t3.micro"
 
-
   tags = {
     Name = "Terraform-First-EC2"
   }
+}
+
+resource "aws_ec2_instance_state" "web_state" {
+  instance_id = "i-05260073427c17ea7"
+  # Change it from "stopped" to STOP and "running" to start
+  state = "stopped"
 }
